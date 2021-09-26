e5aa788e8896f482259dd64cdc63dd726781a029
Ibrahim Abdulhamid <freshiro@gmail.com>
2021-09-27 00:52:43 +0100

    using Javascript to display data

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
+const imageContainer = document.getElementById('image-container'); 
+const loader = document.getElementById('loader'); 
+
+let photosarray = [];// let allows variables to change 
+
 //Unsplash API 
-const count =10; //Number of photos to return
+const count =30; //Number of photos to return
 const apiKey= 'WpNj8CgocgeyAxAxzlEfkMp5tzoEpVfbkRESwuSPRMg';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;// filter in the get request to retrieve random pictures 
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;// filter in the GET request to retrieve random pictures `
+
+
+//Create Elements for Links and photos, add to DOM 
+function displayPhotos(){
+    photosarray.forEach( (photo) =>{
+        //Create <a> to link to Unsplash
+        const link = document.createElement('a');
+        link.setAttribute('href', photo.links.html);
+        link.setAttribute('target', '_blank');
+        
+        //Create <img> to link to Unsplash
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular)
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+
+        // Put <img> inside<a>, then put both inside imageContainer Element 
+        link.appendChild(img);
+        imageContainer.appendChild(link);
+
+    });
+}
+
+
+
 
 //Get photos from Unsplash API
 async function getphotos(){
     try {
         const response = await fetch(apiUrl); //await request from api url 
-        const data = await response.json(); // the response will run thruough the json message and returned as json 
-        console.log(data);
+      photosarray= await response.json(); // the response will run thruough the json message and returned as json 
+        displayPhotos();
     } catch (error) {
         // catch Error Here 
     }
