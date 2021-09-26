083ac6973e70cd210914d899004716a8595711fe
Ibrahim Abdulhamid <freshiro@gmail.com>
2021-09-26 18:05:04 +0100

    Getting Photos from Unsplash API

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 //Unsplash API 
-const apiUrl = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`;
+const count =10; //Number of photos to return
+const apiKey= 'WpNj8CgocgeyAxAxzlEfkMp5tzoEpVfbkRESwuSPRMg';
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;// filter in the get request to retrieve random pictures 
+
+//Get photos from Unsplash API
+async function getphotos(){
+    try {
+        const response = await fetch(apiUrl); //await request from api url 
+        const data = await response.json(); // the response will run thruough the json message and returned as json 
+        console.log(data);
+    } catch (error) {
+        // catch Error Here 
+    }
+}
+// On load 
+getphotos();
